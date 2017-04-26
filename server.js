b3569754536a6a8d5ce6065ce96fc1f54b41a004
Michael Kinkaid <michaelkinkaid@gmail.com>
2017-04-26 17:39:41 -0400

    Initial setup with actions-on-google.

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const Assistant = require('actions-on-google').ApiAiAssistant;
+
+const NAME_ACTION = 'make_name';
+const COLOR_ARGUMENT = 'color';
+const NUMBER_ARGUMENT = 'number';
+
+// [START SillyNameMaker]
+exports.sillyNameMaker = (req, res) => {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Make a silly name
+  function makeName (assistant) {
+    let number = assistant.getArgument(NUMBER_ARGUMENT);
+    let color = assistant.getArgument(COLOR_ARGUMENT);
+    assistant.tell('Alright, your silly name is ' +
+      color + ' ' + number +
+      '! I hope you like it. See you next time.');
+  }
+
+  let actionMap = new Map();
+  actionMap.set(NAME_ACTION, makeName);
+
+  assistant.handleRequest(actionMap);
+};
