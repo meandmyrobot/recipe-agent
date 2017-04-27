3571020fce8f10d121507978839553d8cef27329
Michael Kinkaid <michaelkinkaid@gmail.com>
2017-04-27 16:44:58 -0400

    A meaningless commit to see if the file deletion worked as intended.

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const params = require('./params');
+
+// Load params from environment
 let projectId = process.env.KENTICO_CLOUD_PROJECT_ID || params.projectId;
 let apiKey = process.env.KENTICO_CLOUD_API_KEY || params.apiKey;
+
+// Developer mode only works on local
 let devMode = false || params.apiKey;
+
 module.exports = {apiKey, devMode, projectId};
