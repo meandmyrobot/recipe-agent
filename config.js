f32850d55c2223595c86229024d64f85a5d101d6
Michael Kinkaid <michaelkinkaid@gmail.com>
2017-04-27 16:59:22 -0400

    More playing around with private variables. Not checking in any e2e tests as I don't want API keys in code - which means they don't get to Travis CI.

--- config.js
+++ config.js
@@ -1,17 +1,7 @@
-let params = null;
-
-try {
-    params = require('./params');
-}
-catch(err) {
-    console.log(err);
-}
+let params = require('./params');
 
 // Load params from environment
 let projectId = process.env.KENTICO_CLOUD_PROJECT_ID || params.projectId;
 let apiKey = process.env.KENTICO_CLOUD_API_KEY || params.apiKey;
 
-// Developer mode only works on local
-let devMode = false || params.apiKey;
-
-module.exports = {apiKey, devMode, projectId};
+module.exports = {apiKey, projectId};
