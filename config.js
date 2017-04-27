11bb7d04235dcd69ae858728b67cf22d89f86ccd
Michael Kinkaid <michaelkinkaid@gmail.com>
2017-04-27 16:32:21 -0400

    Changing keys.js to params.

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-const Keys = require('./keys');
-let projectId = process.env.KENTICO_CLOUD_PROJECT_ID || Keys.projectId;
-let apiKey = process.env.KENTICO_CLOUD_API_KEY || Keys.apiKey;
-let devMode = false || Keys.apiKey;
+const params = require('./params');
+let projectId = process.env.KENTICO_CLOUD_PROJECT_ID || params.projectId;
+let apiKey = process.env.KENTICO_CLOUD_API_KEY || params.apiKey;
+let devMode = false || params.apiKey;
 module.exports = {apiKey, devMode, projectId};
-
