1391d8493c1b8939fef08be23ddfe0495b7fd303
Michael Kinkaid <michaelkinkaid@gmail.com>
2017-05-04 13:51:11 -0400

    Pushing a build to see what is up

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Constants
-const express = require('express');
-const bodyParser = require('body-parser');
+let express = require('express');
+let bodyParser = require('body-parser');
 const Assistant = require('actions-on-google').ApiAiAssistant;
 const RecipeAgent = require('./recipe_agent/recipe-agent');
 
@@ -12,16 +12,16 @@ app.use(bodyParser.json());
 const devPort = 5000;
 let port = process.env.PORT || devPort;
 
+// Provide a response for browser hitting the app. TODO: - output recipe landing page
+app.get('/', (req, res) => {
+    res.send('Server is up and running.');
+});
+
 // Fulfillment URL endpoint for Google
 app.post('/api', (req, res) => {
     const assistant = new Assistant({request: req, response: res});
     const recipeAgent = new RecipeAgent(assistant);
     recipeAgent.broadcastResponseFromIntent();
-});
-
-// Provide a response for browser hitting the app. TODO: - output recipe landing page
-app.get('/', (req, res) => {
-    res.send('Server is up and running.');
 });
 
 // Log out what port we are on
